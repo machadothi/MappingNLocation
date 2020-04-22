86aa6fa581ccb90b3d3ed78b802271e748f765ce
Thiago Machado <thiago.machado@pitec.co>
2020-04-21 21:23:13 -0300

    Script converted successfully

--- Modelo Inverso do Sensor II/plotinvsensor2.py
+++ Modelo Inverso do Sensor II/plotinvsensor2.py
@@ -9,78 +9,82 @@ import matplotlib.pyplot as plt
 
 def plotinvsensor():
 
-  # tamanho da celula
-  tamCel = 50     # mm
-  # numero de celulas
-  numCelX = 200     # 10 x 10 m
-  numCelY = 200
-  # mapa
-  mapa = np.zeros(numCelX, numCelY)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    # tamanho da celula
+    tamCel = 50         # mm
+    # numero de celulas
+    numCelX = 200         # 10 x 10 m
+    numCelY = 200
+    # mapa
+    mapa = np.zeros((numCelX, numCelY))
 
-  # pose do robo
-  px = 3000
-  py = 1500
-  pth = 15*np.pi/180
+    # pose do robo
+    px = 3000
+    py = 1500
+    pth = 15*np.pi/180
 
-  # leitura do laser
-  range_value = 2000
-  fi = 20*pi/180
-  mu = np.array([range_value, fi])
+    # leitura do laser
+    range_value = 2000
+    fi = 20*np.pi/180
+    mu = np.array([range_value, fi])
 
-  # dados do laser
-  precisao = 50  # mm
-  passo = 1*pi/180   # rad
+    # dados do laser
+    precisao = 50    # mm
+    passo = 1*np.pi/180     # rad
 
-  sigma = np.array([[2, 0],[0, 0.2]])   # altera o shape da curva
-# sigma = [0.5 0 ; 0 0.05];   # altera o shape da curva
-# sigma = [0.05 0 ; 0 0.005];   # altera o shape da curva
-  invSigma = np.linalg.inv(sigma)
-  Pmin = 0.4
+    sigma = np.array([[2, 0],[0, 0.2]])     # altera o shape da curva
+# sigma = [0.5 0 ; 0 0.05];     # altera o shape da curva
+# sigma = [0.05 0 ; 0 0.005];     # altera o shape da curva
+    invSigma = np.linalg.inv(sigma)
+    Pmin = 0.4
 
-  K = 0.5   # altera o maximo da curva (0.7-0.8)
-#  K = 0.02;   # altera o maximo da curva (0.7-0.8)
-#  K = 0.0002;   # altera o maximo da curva
+    K = 0.5     # altera o maximo da curva (0.7-0.8)
+#    K = 0.02;     # altera o maximo da curva (0.7-0.8)
+#    K = 0.0002;     # altera o maximo da curva
 
-  x = np.linspace(1, numCelX, numCelX)  # X
-  y = np.linspace(1, numCelY, numCelY)  # Y
-  xx, yy = np.meshgrid(x, y)
-  max_value = 0
+    x = np.linspace(1, numCelX, numCelX)    # X
+    y = np.linspace(1, numCelY, numCelY)    # Y
+    xx, yy = np.meshgrid(x, y)
+    max_value = 0
 
-  z = np.zeros(len(y), len(x))
-  for i in range(len(x)):
-    # posicao da celula (i,j) no referencial global
-    xi = (i-1)*tamCel + tamCel/2
-    for j in range(len(y)):
-       yj = (j-1)*tamCel + tamCel/2
-       # raio em relacao ao robo
-       r = np.sqrt((xi-px)**2 + (yj-py)**2)
-       # angulo em relacao ao robo
-       b = np.arctan2((yj-py), (xi-px)) - pth
-       # diferença em relacao a leitura do sensor
-       delta = np.array([r, b]) - mu
-       if abs(delta(2)) > 2*passo
-#       if abs(delta(2)) > 2*passo || abs(delta(1)) > 2*precisao
-         continue;
-       end
-       if r < mu(1)
-          P = Pmin
-       else
-          P = 0.5
-       end
-       delta(1) = delta(1)/1000  # distancia em metros
-       z(i,j) = P + (K/(2*pi*sigma(1,1)*sigma(2,2)) + 0.5-P)*exp(-0.5*delta*invSigma*delta')
-       mapa(i,j) = mapa(i,j) + log(z(i,j)/(1-z(i,j)))
-       if z(i,j) > max
-          max = z(i,j)
-       end
-     end
-  end
-  max
+    z = np.zeros((len(y), len(x)))
+    for i in range(len(x)):
+        # posicao da celula (i,j) no referencial global
+        xi = (i-1)*tamCel + tamCel/2
+        for j in range(len(y)):
+            yj = (j-1)*tamCel + tamCel/2
+            # raio em relacao ao robo
+            r = np.sqrt((xi-px)**2 + (yj-py)**2)
+            # angulo em relacao ao robo
+            b = np.arctan2((yj-py), (xi-px)) - pth
+            # diferença em relacao a leitura do sensor
+            delta = (np.array([r, b]) - mu)
+            if abs(delta[1]) > 2*passo:
+#           if abs(delta(2)) > 2*passo || abs(delta(1)) > 2*precisao
+                continue
+                 
+            if r < mu[0]:
+                P = Pmin
+            else:
+                P = 0.5
 
-  surf(xx,yy,z')
-  xlabel('X')
-  ylabel('Y')
-  zlabel('p(x|z,theta)')
-  colormap(hsv)
+            delta[0] /= 1000  # distancia em metros
+            dummy = np.dot(delta,invSigma)
+            dummy2 = np.dot(dummy , delta.T)
+            z[i, j] =  P + (K/(2*np.pi*sigma[0,0]*sigma[1 ,1]) + 0.5 - P)*np.exp(-.5 *dummy2)
+            mapa[i,j] = mapa[i,j] + np.log(z[i,j]/(1-z[i,j]))
+            if z[i,j] > max_value:
+                max_value = z[i,j]
 
-end
+    print(max_value)
+    ax.plot_surface(xx, yy, z.T, cmap='viridis', edgecolor='none')
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('p(x|z,theta)')
+    plt.show()
+
+
+if __name__ == "__main__":
+    
+    plotinvsensor()
